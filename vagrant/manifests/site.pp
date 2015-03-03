f940b71d4bca3c19531cec08da3ea33c85959b17
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2015-03-03 11:41:01 +0100

    Extends puppetfile with modules, adds basic vagrant provisioning
    * sets up elasticsearch

--- vagrant/manifests/site.pp
+++ vagrant/manifests/site.pp
@@ -1,4 +1,32 @@
 node default {
   include vim
   include git
+  include java
 }
+
+class { 'ruby':
+  version => '2.0.0'
+}
+
+class { 'ruby::dev': }
+
+class { "elasticsearch":
+  status       => 'enabled',
+  manage_repo  => true,
+  repo_version => '1.4',
+  version      => '1.4.2',
+  config       => {
+    "path.conf"                   => "/etc/elasticsearch",
+    "path.data"                   => "/var/lib/elasticsearch",
+    "gateway.expected_nodes"      => 1,
+    "gateway.recover_after_nodes" => 1
+  }
+}
+
+elasticsearch::instance { 'primary':
+  ensure => present,
+  status => 'enabled',
+  config => { "node.name" => "el-example" }
+}
+
+Package['java'] -> Package['elasticsearch']

5119f96b753e9f3fcfab45aaaf22b31276bb889b
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2015-03-04 12:11:41 +0100

    Parses genres and movie information from file
    * also adds the dataset as a gem to the local repository, making it available for use in the Rakefile

--- dataset/movies-100k/lib/example/data_set_loader.rb
+++ dataset/movies-100k/lib/example/data_set_loader.rb
@@ -3,6 +3,7 @@ require 'pry'
 module Example
   class DataSetLoader
     attr_reader :dataset_dir
+    attr_reader :genres
 
     def initialize(dataset_dir)
       @dataset_dir = dataset_dir
@@ -13,26 +14,29 @@ module Example
       result = {}
       File.open(File.join(dataset_dir, filename)) do |file|
         file.each_line do |line|
-          title, index  = line.split('|')
-          result[index] = title
+          title, index  = line.chomp.split('|')
+          result[Integer(index)] = title if title && index
         end
       end
       result
     end
 
     def create_movie_seed_file(input_file, output_file)
-      File.open(File.join(dataset_dir, input_file), 'r') do |file|
+      File.open(File.join(dataset_dir, input_file), 'r:iso-8859-1') do |file|
         file.each_line do |line|
-          components = line.split("\t")
+          components = line.chomp.split("|")
           id           = components[0]
           title        = components[1]
           release_date = components[2]
           video_date   = components[3]
           imdb_url     = components[4]
-          genre        = components[5..-1].map{ || }
-          binding.pry
+          genre        = detect_genres(components[5..-1].map{ |i| Integer(i) })
         end
       end
+    end
+
+    def detect_genres(list)
+      list.each_index.select{ |i| list[i] > 0 }
     end
   end
 end
